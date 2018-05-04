d4be31518c56f1e601fe2b8c50dda8467d379afb
andpago <andpago@gmail.com>
2018-05-04 13:58:50 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: './src/index.js',
+    
+    output: {
+        path: '/home/andpago/Desktop/tournament/static/',
+        filename: 'index.js'
+    },
+    
+    mode: 'development'
+};
