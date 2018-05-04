1d7ae2f7264ce53049c049e282401c45fd960c14
andpago <andpago@gmail.com>
2018-05-04 15:20:48 +0300

    react

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App(props) {
+    return (<div>HELLO REACT</div>);
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     
     output: {
         path: '/home/andpago/Desktop/tournament/static/',
         filename: 'index.js'
     },
     
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: `${__dirname}/src`,
+                loader: 'babel-loader?presets[]=react&presets[]=env&presets[]=stage-1',
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded',
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+                loader: 'url-loader?limit=4096&name=[path][name].[ext]',
+            }
+        ]
+    },
+    
     mode: 'development'
 };
+
