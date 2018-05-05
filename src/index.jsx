53308b277ea3f9e7035afde4a5977f8786c5beca
andpago <andpago@gmail.com>
2018-05-05 13:07:41 +0300

    bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { initStore } from './util/store';
 import { Provider } from 'react-redux';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const store = initStore([]);
 
 function App(props) {
     return (
     	<Provider store={ store }>
-    		<div>HELLO REACT</div>
+    		<div>
+                HELLO REACT
+                <button className="btn btn-success"> Button </button>
+            </div>
     	</Provider>
     );
 }
