9b752b905cc0f71920eae928943dbc6f2a74f460
andpago <andpago@gmail.com>
2018-05-12 17:42:15 +0300

    login form invalid indication

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -39,12 +39,22 @@ class LoginForm extends React.Component {
 		}).then((res) => {
 			if (res.status === 200) {
 				res.json().then(json => {
-					console.log('logged in as ' + this.state.login);
 					Cookies.set('key', json.key);
 
 					this.loginWithToken(json.key);
 				});
 			} else {
+				if (res.status === 400) {
+					res.json().then(json => {
+						if (json.hasOwnProperty('non_field_errors') && 
+							json.non_field_errors[0] == 'Unable to log in with provided credentials.') {
+							this.setState({
+								valid: false,
+							});
+						}
+					});
+				}
+
 				console.log('failed to login');
 			}
 		});
@@ -52,32 +62,27 @@ class LoginForm extends React.Component {
 		event.preventDefault();
 	}
 
-	changeLogin(event) {
-		this.state = {
-			login: event.target.value,
-		}
-	}
-
 	constructor(props) {
 		super(props);
 		this.state = {
-			login: '',
+			valid: true,
 		}
 		this.sendForm = this.sendForm.bind(this);
-		this.changeLogin = this.changeLogin.bind(this);
 		this.loginWithToken = this.loginWithToken.bind(this);
 	}
 
 	render() {
+		const validClass = this.state.valid ? '' : 'is-invalid';
+
 		return (
 				<form id="login-form" className="form-inline my-2 my-lg-0" action="/rest-auth/login/" method="POST">
-					<input  className="form-control mr-sm-2" 
+					<input  className={"form-control mr-sm-2 " + validClass}
 							name="username" 
 							type="input" 
 							placeholder="логин" 
 							onChange={ this.changeLogin }
 							aria-label="логин" />
-					<input  className="form-control mr-sm-2" 
+					<input  className={"form-control mr-sm-2 " + validClass}
 							name="password" 
 							type="password"
 							placeholder="пароль" 
